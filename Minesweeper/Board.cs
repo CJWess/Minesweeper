3f6a3cb29d52dd38a778484e26423aa1a8926ebb
ChaseWessman <chase.wessman@regalcuttingtools.com>
2020-01-07 15:59:40 -0600

    Add project files.

---
+++ Minesweeper/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class Board
+    {
+        public int Height { get; }
+        public int Width { get; }
+
+        private Dictionary<Point, Tile> tiles;
+        
+        public List<Tile> NeighboringTiles(Tile tile)
+        {
+            List<Tile> tileList = new List<Tile>();
+            for(int x = tile.location.X - 1; x <= tile.location.X + 1; x++)
+            {
+                for (int y = tile.location.Y - 1; y <= tile.location.Y + 1; y++)
+                {
+                    Point point = new Point(x, y);
+                    if(InBounds(point))
+                    {
+                        tileList.Add(tiles[point]);
+                    }
+                }
+            }
+            return tileList;
+
+        }
+
+        public bool InBounds(Point point)
+        {
+            return point.X < Width && point.X >= 0 && point.Y >= 0 && point.Y < Height;
+        }
+    }
+}
---
+++ Minesweeper/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class Tile
+    {
+        public int NumericValue { get; }
+        public bool IsMine { get; }
+
+        public Point location;
+
+        public Tile(Board board)
+        {
+            NumericValue = GetValue(board);
+        }
+        
+
+        public int GetValue(Board board)
+        {
+            return board.NeighboringTiles(this).Count(x => x.IsMine);
+        }
+    }
+}
